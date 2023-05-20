4d68ada8bca2f55a4f5ca73013abcbadb6283a8f
SiyaNontolwana <76439799+Blizzard0ne@users.noreply.github.com>
2023-05-20 16:17:09 +0200

    Enemy ground work
    Added a enemy class script and began work on bat enemy.

---
+++ Assets/Scripts/EnemyScripts/EnemyClass.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyClass
+{
+    public string name;
+    public int health;
+    public float speed;
+    public float attackDamage;
+    public float attackSpeed;
+
+}
