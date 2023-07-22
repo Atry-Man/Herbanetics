e4dbee346fefab6490458c6b174d28c3de5a6539
Gi-ddo <60913804+Gi-ddo@users.noreply.github.com>
2023-07-23 00:52:40 +0200

    Punch code addition

--- Assets/Scripts/PlayerAttackController.cs
+++ Assets/Scripts/PlayerAttackController.cs
@@ -7,13 +7,23 @@ public class PlayerAttackController : MonoBehaviour
     [Header("Attack Combo Variables")]
     [SerializeField] Animator playerAnim;
     private const string punchStr = "Punch";
- 
+
+    [SerializeField] Transform punchPos;
+    [SerializeField] GameObject punchObj;
+    [SerializeField] float punchForce;
     public void OnBigPunch(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
             playerAnim.SetTrigger(punchStr);
+            ActivatePunch();
         }
     }  
     
+
+    public void ActivatePunch()
+    {
+        GameObject punch = Instantiate(punchObj, punchPos.position, Quaternion.identity);
+        punch.GetComponent<Rigidbody>().AddForce(punchPos.forward * punchForce, ForceMode.Impulse);
+    }
 }
